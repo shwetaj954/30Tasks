1fa391b7d6c3a9d203996e014d5163caf76f977d
Shweta jha <shwetaj954@gmail.com>
2022-01-24 22:17:45 +0530

    g

--- Day3 Fetech API JS/index.js
+++ Day3 Fetech API JS/index.js
@@ -8,3 +8,21 @@ async function getUsers() {
         console.log(error);
     }
 }
+async function renderUsers() {
+    let users = await getUsers();
+    let html = '';
+    users.forEach(user => {
+        let htmlSegment = `<div class="user">
+                            <img src="${user.profileURL}" >
+                            <h2>${user.dailyconfirmed} ${user.date}</h2>
+                            <div class="email"><a href="email:${user.email}">${user.email}</a></div>
+                        </div>`;
+
+        html += htmlSegment;
+    });
+
+    let container = document.querySelector('.container');
+    container.innerHTML = html;
+}
+
+renderUsers();
